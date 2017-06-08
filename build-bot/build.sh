2bbf824215b4e52d773e13e5f90fd1af74ed10a0
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-06-08 20:33:03 +0530

    Complete docker definitions

--- build-bot/build.sh
+++ build-bot/build.sh
@@ -2,12 +2,19 @@
 # Builds a sugar activity, accpets repo_name as parameter
 set -e
 # Temporary bash activation
-source "$HOME/venvs/sugar-dev/bin/activate"
+#source "$HOME/venvs/sugar-dev/bin/activate"
 target_repo=$1
-echo $VIRTUAL_ENV
+#echo $VIRTUAL_ENV
 echo "Target repo is $target_repo"
-cd "repos/$target_repo"
+cd "/activities/$target_repo"
+
+echo "Creating Bundle"
 python setup.py dist_xo
-mkdir -p bundles
-mv dist/*.xo "../../bundles/"
-#mv "repos/$target_repo/dist/*.xo" "bundles/"
+#mkdir -p bundles
+#mv dist/*.xo "../../bundles/"
+echo "Moving Bundle"
+mv "dist/*.xo" "/bundles/"
+
+echo "Cleaning up"
+
+rm -rf "activities/$target_repo" 
