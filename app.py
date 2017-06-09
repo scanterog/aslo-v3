53a201c9006aa1d3dd2b3fdb2e38ee35214e2783
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-06-09 17:50:42 +0530

    Clean up unecessary dependencies and migrate from Python 2 to Python 3

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask,request,abort
+from flask import Flask, request, abort
 from celery import Celery
 import os
-from IPython import embed
 from flask_pymongo import PyMongo
 import utils
 
@@ -16,29 +15,31 @@ celery.conf.update(app.config)
 # Set false for production systems
 
 # Create Mongo instance
-#TODO : Secure Mongo to avoid Mongo Ransomware :sweat_smile:
+# TODO : Secure Mongo to avoid Mongo Ransomware :sweat_smile:
 mongo = PyMongo(app)
 
 app.debug = True
 GITHUB_HOOK_SECRET = os.environ.get('GITHUB_HOOK_SECRET')
+
 
 @app.route('/')
 def main():
     embed()
     return "Welcome"
 
-@app.route('/webhook',methods=['POST'])
+
+@app.route('/webhook', methods=['POST'])
 def handle_payload():
     content = request.get_json(silent=True)
     header_signature = request.headers['X-Hub-Signature']
     if header_signature is None:
         abort(403)
-    if not utils.verify_signature(header_signature,request.data,GITHUB_HOOK_SECRET):
+    if not utils.verify_signature(header_signature, request.data, GITHUB_HOOK_SECRET):
         abort(403)
 
     repo_url = content['repository']['clone_url']
     repo_name = content['repository']['name']
-    task = clone_repo.apply_async(args=[repo_url,repo_name])
+    task = clone_repo.apply_async(args=[repo_url, repo_name])
     return "Authenticated request :D"
 
 
@@ -46,21 +47,17 @@ def handle_payload():
 def test_celery():
     repo_url = request.args.get('repo_url')
     repo_name = request.args.get('repo_name')
-    print "Repo name is " + repo_url
-    task = process_build.apply_async(args=[repo_url,repo_name])
+    print("Repo name is " + repo_url)
+    task = process_build.apply_async(args=[repo_url, repo_name])
     return "Done"
 
+
 @celery.task
-def process_build(repo_url,repo_name):
+def process_build(repo_url, repo_name):
     utils.clone_repo(repo_url)
     acitvity_file = utils.check_activity(repo_name)
-    if activity_file not None:
-        parsed_json = utils.convert_to_json_object(utils.read_activity(activity_file))
-        mongo.db.acitivites.insert(parsed_json)
-        utils.build_activity(repo_name)
     return 1
-    #check_activity(repo_name)
-
+    # check_activity(repo_name)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,44 +1,48 @@
 
 import hmac
 import os
-import ConfigParser
+import configparser
 import json
 from hashlib import sha1
 
 
 # Thanks to https://github.com/carlos-jenkins/python-github-webhooks/blob/master/webhooks.py
 # https://developer.github.com/webhooks/securing/
-def verify_signature(header_signature,raw_data,secret):
+def verify_signature(header_signature, raw_data, secret):
     sha_name, signature = header_signature.split('=')
     if sha_name != 'sha1':
-       return false
+        return False
     # HMAC requires the key to be bytes, pass raw request data
-    mac = hmac.new(secret,raw_data,sha1)
+    mac = hmac.new(secret, raw_data, sha1)
     # Use compare_digest to avoid timing attacks
     return hmac.compare_digest(str(mac.hexdigest()), str(signature))
 
+
 def clone_repo(clone_url):
-    print "Cloning repo " + clone_url
+    print("Cloning repo " + clone_url)
     os.system("git -C repos/ clone {} ".format(clone_url))
 
-#TODO : Refactor lose functions to a wrapper class
+# TODO : Refactor lose functions to a wrapper class
+
 
 def check_activity(repo_name):
-    target_folder = os.path.join("repos/",repo_name)
+    target_folder = os.path.join("repos/", repo_name)
     if os.path.exists(target_folder):
-        activity_file = os.path.join(target_folder,"activity/activity.info")
+        activity_file = os.path.join(target_folder, "activity/activity.info")
         return activity_file
     else:
-       return None
+        return None
 
 
 def read_activity(activity_file):
-    parser = ConfigParser.ConfigParser()
+    parser = configParser.ConfigParser()
     parser.read(activity_file)
     return parser
 
-def get_activity_attribute(parser,attribute):
-    return parser.get('Activity',attribute)
+
+def get_activity_attribute(parser, attribute):
+    return parser.get('Activity', attribute)
+
 
 def convert_to_json_string(parser):
     # Get all attrbutes of acitvity
@@ -46,8 +50,10 @@ def convert_to_json_string(parser):
     # Convert attributes to a JSON string
     return json.dumps(dict(attributes))
 
+
 def convert_to_json_object(parser):
     return json.loads(convert_to_json_string(parser))
+
 
 def build_activity(repo_location):
     os.system("./build_activity.sh {}".format(repo_location))
