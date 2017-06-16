0b55dcc914271ef9b7a4ca45393438808083430c
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-06-16 21:20:01 +0530

    Fix docker permission issues by using setfacl

--- build-bot/build.sh
+++ build-bot/build.sh
@@ -7,14 +7,17 @@ target_repo=$1
 #echo $VIRTUAL_ENV
 echo "Target repo is $target_repo"
 cd "/activities/$target_repo"
-
+echo "Bridge USER ID is $LOCAL_USER_ID"
+echo $USER 
 echo "Creating Bundle"
 python setup.py dist_xo
-#mkdir -p bundles
+mkdir -p bundles
 #mv dist/*.xo "../../bundles/"
 echo "Moving Bundle"
-mv dist/*.xo /bundles/
+# Assign target repo 
+setfacl -R -m u:$LOCAL_USER_ID:rwx "/activities/$target_repo"
+cp dist/*.xo /bundles/
 
-echo "Cleaning up"
-
-rm -rf "activities/$target_repo"
+echo "Setting up"
+#setfacl -R -m u:$LOCAL_USER_ID:rwx $target_repo
+#rm -rf "activities/$target_repo"
