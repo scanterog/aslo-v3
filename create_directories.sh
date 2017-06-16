d2f4f59b801ed85a9cdb2e0ea95f402a71b0c77a
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-06-16 21:20:24 +0530

    Use setfacl to create directories

--- create_directories.sh
+++ create_directories.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+current_user=$USER
 echo "Creating directories ....."
 
 echo "Creating directory for storing downloaded repositories"
 mkdir -p /tmp/activities
+#chmod +w -R /tmp/activities
+sudo setfacl -m u:$current_user:rwx /opt/bundles
 
 echo "Creating directories to store downloded bundled activities"
 
 sudo mkdir -p /opt/bundles
-sudo chmod +w -R /opt/bundles/
+#sudo chmod +w -R /opt/bundles/
+sudo setfacl -m u:$current_user:rwx /opt/bundles
+
 
 echo "Done "
